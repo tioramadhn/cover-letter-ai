4b0ad23610c2d620a86791c8cbb49018581a236f
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2024-10-08 17:26:41 +0700

    feat: develop ui

---
+++ src/components/CoverLetterForm.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { formSchema } from "@/validation/formSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Textarea } from "./ui/textarea";
+
+export const CoverLetterForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      jobDetailsType: "text",
+    },
+  });
+
+  const fileCVRef = form.register("fileCV");
+  const jobDetailsFileRef = form.register("jobDetailsFile");
+  const jobDetailsTextRef = form.register("jobDetailsText");
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="fileCV"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>CV / Resume</FormLabel>
+              <FormControl>
+                <Input type="file" {...fileCVRef} />
+              </FormControl>
+              <FormDescription>
+                This is your CV or Resume as base knowledge.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="jobDetailsType"
+          render={({ field }) => (
+            <FormItem className="space-y-4">
+              <FormItem>
+                <FormLabel>Job Detail</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex gap-4"
+                  >
+                    <FormItem className="flex items-center space-x-2 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="text" />
+                      </FormControl>
+                      <span className="font-normal text-sm">Text</span>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-2 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="file" />
+                      </FormControl>
+                      <span className="font-normal text-sm">File</span>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+
+              {field.value === "file" && (
+                <FormItem>
+                  <FormControl>
+                    <Input type="file" {...jobDetailsFileRef} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+
+              {field.value === "text" && (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Tell us a little bit about yourself"
+                      // className="resize-none"
+                      {...jobDetailsTextRef}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+              <FormDescription>
+                This is your Job Details information to generate your cover
+                letter.
+              </FormDescription>
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          Generate
+        </Button>
+      </form>
+    </Form>
+  );
+};
---
+++ src/validation/formSchema.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+
+export const formSchema = z.object({
+  fileCV: z
+    .instanceof(FileList)
+    .refine((files) => files && files?.length > 0, {
+      message: "Please upload your CV.",
+    })
+    .transform((files) => files[0])
+    .pipe(
+      z
+        .instanceof(File)
+        .refine(
+          (file) =>
+            ["image/jpeg", "image/png", "application/pdf"].includes(file.type),
+          {
+            message: "File type must be JPEG, PNG, or PDF",
+          }
+        )
+    ),
+  jobDetailsType: z.string(),
+  jobDetailsFile: z
+    .instanceof(FileList)
+    .refine((files) => files && files?.length > 0, {
+      message: "Please upload your Job Details.",
+    })
+    .transform((files) => files[0])
+    .pipe(
+      z
+        .instanceof(File)
+        .refine(
+          (file) =>
+            ["image/jpeg", "image/png", "application/pdf"].includes(file.type),
+          {
+            message: "File type must be JPEG, PNG, or PDF",
+          }
+        )
+    ),
+  jobDetailsText: z.string().optional(),
+});
